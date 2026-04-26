09b259fb46ab855d75697b798b44e68d89260f98
Kabya55 <kabyakishor017@gmail.com>
2026-04-27 01:09:26 +0600

    Drogon news

--- Modul-46-dragon-news-next/src/proxy.js
+++ Modul-46-dragon-news-next/src/proxy.js
@@ -9,7 +9,16 @@ export async function proxy(request) {
   });
 
   if (!session) {
-    return NextResponse.redirect(new URL("/login", request.url));
+    // return NextResponse.redirect(new URL("/login", request.url));
+
+    // ১. ইউজার বর্তমানে যে পেজে যাওয়ার চেষ্টা করছে তার URL নিন
+    const currentUrl = request.nextUrl.pathname;
+
+    // ২. লগইন পেজে পাঠানোর সময় callbackUrl হিসেবে বর্তমান পেজটি পাঠিয়ে দিন
+    const loginUrl = new URL("/login", request.url);
+    loginUrl.searchParams.set("callbackUrl", currentUrl);
+
+    return NextResponse.redirect(loginUrl);
   }
 
   // If the user is authenticated, you can proceed with the request
