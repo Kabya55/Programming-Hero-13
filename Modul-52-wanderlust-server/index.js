98a8177942640911097feae0527ac8073093f233
Kabya55 <kabyakishor017@gmail.com>
2026-05-16 00:16:38 +0600

    wanderlust-server

--- Modul-52-wanderlust-server/index.js
+++ Modul-52-wanderlust-server/index.js
@@ -8,6 +8,7 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { createRemoteJWKSet, jwtVerify } = require("jose-cjs");
 
 const uri = process.env.MONGO_URI;
 
@@ -19,19 +20,43 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+
+const JWKS = createRemoteJWKSet(
+  new URL(`${process.env.CLIENT_URL}/.well-known/jwks.json`),
+);
+const veryfyToken = async (req, res, next) => {
+  const authHeader = req?.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  const token = authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const { payload } = await jwtVerify(token, JWKS);
+    // console.log(payload);
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const db = client.db("wanderlust");
     const destinationsCollection = db.collection("destinations");
+    const bookingsCollection = db.collection("bookings");
 
     app.get("/destinations", async (req, res) => {
       const result = await destinationsCollection.find().toArray();
       res.json(result);
     });
 
-    app.get("/destinations/:id", async (req, res) => {
+    app.get("/destinations/:id", veryfyToken, async (req, res) => {
       const { id } = req.params;
 
       const result = await destinationsCollection.findOne({
@@ -40,10 +65,9 @@ async function run() {
       res.json(result);
     });
 
-    app.patch("/destinations/:id", async (req, res) => {
+    app.patch("/destinations/:id", veryfyToken, async (req, res) => {
       const { id } = req.params;
       const updatedData = req.body;
-      console.log(updatedData);
 
       const result = await destinationsCollection.updateOne(
         { _id: new ObjectId(id) },
@@ -52,15 +76,37 @@ async function run() {
       res.json(result);
     });
 
-    app.post("/destinations", async (req, res) => {
+    app.post("/destinations", veryfyToken, async (req, res) => {
       const newDestination = req.body;
       const result = await destinationsCollection.insertOne(newDestination);
       res.json(result);
     });
 
-    app.delete("/destinations/:id", async (req, res) => {
+    app.delete("/destinations/:id", veryfyToken, async (req, res) => {
       const { id } = req.params;
       const result = await destinationsCollection.deleteOne({
+        _id: new ObjectId(id),
+      });
+      res.json(result);
+    });
+
+    app.get("/bookings/:userId", veryfyToken, async (req, res) => {
+      const { userId } = req.params;
+      const result = await bookingsCollection
+        .find({ userId: userId })
+        .toArray();
+      res.json(result);
+    });
+
+    app.post("/bookings", veryfyToken, async (req, res) => {
+      const newBooking = req.body;
+      const result = await bookingsCollection.insertOne(newBooking);
+      res.json(result);
+    });
+
+    app.delete("/bookings/:id", veryfyToken, async (req, res) => {
+      const { id } = req.params;
+      const result = await bookingsCollection.deleteOne({
         _id: new ObjectId(id),
       });
       res.json(result);
